fa1d23133482841c9c868260e6833162e265007f
yjshin-cloud <yjshin.cloud@gmail.com>
2025-08-17 22:52:01 +0900

    Update main.go

--- 2_Functions_Part1/main.go
+++ 2_Functions_Part1/main.go
@@ -9,7 +9,13 @@ func lenAndUpper(name string) (int, string) {
 	return len(name), strings.ToUpper(name)
 }
 
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 func main() {
+	repeatMe("nico", "lynn", "dal", "marl", "flynn")
+
 	totalLenght, upperName := lenAndUpper("nico")
 	fmt.Println(totalLenght, upperName)
 }
