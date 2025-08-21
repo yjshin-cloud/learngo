5b6a1dcd95842a4ea7ad16c9f20397c64ac280ac
yjshin-cloud <yjshin.cloud@gmail.com>
2025-08-21 19:28:34 +0900

    Create main.go

---
+++ 6_Switch/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func canIDrink(age int) bool {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+
+}
+
+func main() {
+	fmt.Println(canIDrink(18))
+}
